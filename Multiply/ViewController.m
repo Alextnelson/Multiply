bfc23df78c3983dd5f47a9c24785749c6f8f5c36
Alextnelson <alexander.thomas.nelson@gmail.com>
2016-01-28 15:58:44 -0600

    Updated with all local changes

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+@property (weak, nonatomic) IBOutlet UILabel *multiplierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *operatorSegmentControl;
+
+@property NSArray *sliderNumbers;
 
 @end
 
@@ -16,12 +23,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(IBAction)sliderValueChanged:(id)sender {
+    float valueFloat = self.slider.value;
+    int valueInt = (int)valueFloat;
+    self.multiplierLabel.text = [NSString stringWithFormat:@"%i", valueInt];
 }
+
+
+- (IBAction)onCalculateButtonPressed:(id)sender {
+    int x = [self.numberTextField.text intValue];
+    int y = [self.multiplierLabel.text intValue];
+    int result;
+
+    if (self.operatorSegmentControl.selectedSegmentIndex == 0) {
+        result = x * y;
+    } else if (self.operatorSegmentControl.selectedSegmentIndex == 1) {
+        result = x/y;
+    }
+
+    if (result >= 20) {
+        self.view.backgroundColor = [UIColor greenColor];
+    } else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+
+    if (result % 3 == 0 && result % 5 == 0) {
+        self.answerLabel.text = @"Fizzbuzz";
+    } else if (result % 3 == 0) {
+        self.answerLabel.text = @"Fizz";
+    } else if (result % 5 == 0) {
+        self.answerLabel.text = @"Buzz";
+    } else {
+        self.answerLabel.text = [NSString stringWithFormat:@"%i", result];
+    }
+    [self resignFirstResponder];
+}
+
 
 @end
